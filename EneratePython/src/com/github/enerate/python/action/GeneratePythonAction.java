4593d391d9bc6060cdd806de17d67516d1e99ca0
Richard W. Lincoln <r.w.lincoln@gmail.com>
2009-10-26 19:16:49 +0000

    Adding JET variables to transform call.

--- EneratePython/src/com/github/enerate/python/action/GeneratePythonAction.java
+++ EneratePython/src/com/github/enerate/python/action/GeneratePythonAction.java
@@ -4,6 +4,7 @@ import java.util.HashMap;
 
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Path;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.SubProgressMonitor;
@@ -24,6 +25,7 @@ import org.eclipse.emf.codegen.ecore.genmodel.GenModel;
 import org.eclipse.jet.JET2Platform;
 
 public class GeneratePythonAction extends ActionDelegate implements IActionDelegate {
+
 	protected static final URI PLATFORM_RESOURCE = URI.createPlatformResourceURI("/", false);
 
 	protected GenModel genModel;
@@ -36,11 +38,20 @@ public class GeneratePythonAction extends ActionDelegate implements IActionDeleg
 
 	@Override
 	public void run(IAction action) {
+
+		System.out.println("Generating Python Code");
+
 		URI uri = genModel.eResource().getURI();
+
+		System.out.println("GenModel URI: " + uri.toString());
+
 		IStructuredSelection selection = StructuredSelection.EMPTY;
 		if (uri != null && uri.isHierarchical()) {
 			if (uri.isRelative() || (uri = uri.deresolve(PLATFORM_RESOURCE)).isRelative()) {
 				IFile file = ResourcesPlugin.getWorkspace().getRoot().getFile(new Path(uri.toString()));
+
+				System.out.println("File: " + uri.segment(0));
+
 				if (file.exists()) {
 					selection = new StructuredSelection(file);
 				}
@@ -48,21 +59,35 @@ public class GeneratePythonAction extends ActionDelegate implements IActionDeleg
 		}
 
 		Object object = ((IStructuredSelection)selection).getFirstElement();
-		if (object instanceof GenModel) {
-			genModel = (GenModel)object;
+
+		System.out.println("Object: " + object.getClass().toString());
+
+		if (true){//object instanceof GenModel) {
+//			genModel = (GenModel)object;
 
 			HashMap<String, String> variables = new HashMap<String, String>();
-//			variables.put("org.eclipse.jet.resource.project.name", "theNameOfTheProjectContainingTheEMFResource");
-			JET2Platform.runTransformOnObject("com.github.enerate.python", genModel.eResource(), variables, monitor);
+			variables.put("org.eclipse.jet.resource.project.name", uri.segment(0)); // theNameOfTheProjectContainingTheEMFResource
+
+			System.out.println("Transforming: " + genModel.eResource().toString());
+
+			IStatus status = JET2Platform.runTransformOnObject("com.github.enerate.python", genModel.eResource(), variables, monitor);
+
+			if (!status.isOK())
+				System.err.println("JET transformation failed! " + status.getMessage());
+//				Activator.getDefault().getLog().log(status);
 		}
 	}
 
 	@Override
 	public void selectionChanged(IAction action, ISelection selection) {
+
+		System.out.println("Selection changed: " + selection.toString());
+
 		if (selection instanceof IStructuredSelection) {
 			Object object = ((IStructuredSelection)selection).getFirstElement();
 			if (object instanceof GenModel) {
 				action.setEnabled(true);
+				genModel = (GenModel)object;
 				return;
 			}
 		}
