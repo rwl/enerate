71352ffc0b74fa0f2dbf55722acfe3f155fdccee
Richard W. Lincoln <r.w.lincoln@gmail.com>
2009-10-27 11:05:23 +0000

    Implementing ECore resource action.

--- EneratePython/src/com/github/enerate/python/action/GeneratePythonAction.java
+++ EneratePython/src/com/github/enerate/python/action/GeneratePythonAction.java
@@ -1,97 +1,81 @@
 package com.github.enerate.python.action;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
 
 import org.eclipse.core.resources.IFile;
-import org.eclipse.core.resources.ResourcesPlugin;
-import org.eclipse.core.runtime.IStatus;
-import org.eclipse.core.runtime.Path;
+import org.eclipse.core.resources.IProject;
 import org.eclipse.core.runtime.IProgressMonitor;
-import org.eclipse.core.runtime.SubProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.NullProgressMonitor;
-import org.eclipse.emf.common.util.BasicMonitor;
+import org.eclipse.jet.JET2Platform;
 import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.IStructuredSelection;
-import org.eclipse.jface.viewers.StructuredSelection;
-import org.eclipse.jface.dialogs.ProgressMonitorDialog;
+import org.eclipse.swt.widgets.Shell;
 import org.eclipse.ui.IActionDelegate;
-import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.actions.ActionDelegate;
 
-import org.eclipse.emf.common.util.URI;
-import org.eclipse.emf.codegen.ecore.genmodel.GenModel;
-
-import org.eclipse.jet.JET2Platform;
+import com.github.enerate.python.Activator;
 
 public class GeneratePythonAction extends ActionDelegate implements IActionDelegate {
 
-	protected static final URI PLATFORM_RESOURCE = URI.createPlatformResourceURI("/", false);
+	/** The list of selected ECore files. */
+	private ArrayList<IFile> ecoreFiles = new ArrayList<IFile>();
 
-	protected GenModel genModel;
-
+	/** Progress monitor for the transformation. */
 	IProgressMonitor monitor = new NullProgressMonitor();
 
+	/**
+	 * Constructor for GeneratePythonAction.
+	 */
 	public GeneratePythonAction() {
 		super();
 	}
 
+	/**
+	 * @see IActionDelegate#run(IAction)
+	 */
 	@Override
 	public void run(IAction action) {
 
-		System.out.println("Generating Python Code");
-
-		URI uri = genModel.eResource().getURI();
-
-		System.out.println("GenModel URI: " + uri.toString());
-
-		IStructuredSelection selection = StructuredSelection.EMPTY;
-		if (uri != null && uri.isHierarchical()) {
-			if (uri.isRelative() || (uri = uri.deresolve(PLATFORM_RESOURCE)).isRelative()) {
-				IFile file = ResourcesPlugin.getWorkspace().getRoot().getFile(new Path(uri.toString()));
-
-				System.out.println("File: " + uri.segment(0));
-
-				if (file.exists()) {
-					selection = new StructuredSelection(file);
-				}
-			}
+		if (ecoreFiles.size() != 1) {
+			Shell shell = new Shell();
+			MessageDialog.openInformation(shell, "Enerate", "Please select one ECore file.");
+			return;
 		}
 
-		Object object = ((IStructuredSelection)selection).getFirstElement();
+		IFile ecoreFile = ecoreFiles.get(0);
 
-		System.out.println("Object: " + object.getClass().toString());
+		IProject project = ecoreFile.getProject();
+		HashMap<String, String> variables = new HashMap<String, String>();
+		variables.put("org.eclipse.jet.resource.project.name", project.getName()); // theNameOfTheProjectContainingTheEMFResource
 
-		if (true){//object instanceof GenModel) {
-//			genModel = (GenModel)object;
+		IStatus status = JET2Platform.runTransformOnResource("com.github.enerate.python", ecoreFile, variables, monitor);
 
-			HashMap<String, String> variables = new HashMap<String, String>();
-			variables.put("org.eclipse.jet.resource.project.name", uri.segment(0)); // theNameOfTheProjectContainingTheEMFResource
-
-			System.out.println("Transforming: " + genModel.eResource().toString());
-
-			IStatus status = JET2Platform.runTransformOnObject("com.github.enerate.python", genModel.eResource(), variables, monitor);
-
-			if (!status.isOK())
-				System.err.println("JET transformation failed! " + status.getMessage());
-//				Activator.getDefault().getLog().log(status);
-		}
+		if (!status.isOK())
+			Activator.getDefault().getLog().log(status);
 	}
 
+	/**
+	 * @see org.eclipse.ui.actions.ActionDelegate#selectionChanged(org.eclipse.jface.action.IAction, org.eclipse.jface.viewers.ISelection)
+	 */
 	@Override
 	public void selectionChanged(IAction action, ISelection selection) {
 
-		System.out.println("Selection changed: " + selection.toString());
+		if (!(selection instanceof IStructuredSelection))
+			return;
 
-		if (selection instanceof IStructuredSelection) {
-			Object object = ((IStructuredSelection)selection).getFirstElement();
-			if (object instanceof GenModel) {
-				action.setEnabled(true);
-				genModel = (GenModel)object;
-				return;
+		final IStructuredSelection structuredSelection = (IStructuredSelection) selection;
+		final Iterator<?> it = structuredSelection.iterator();
+		ecoreFiles = new ArrayList<IFile>();
+		while (it.hasNext()) {
+			final Object obj = it.next();
+			if (obj instanceof IFile) {
+				ecoreFiles.add((IFile) obj);
 			}
 		}
-		genModel = null;
-		action.setEnabled(false);
 	}
 }
